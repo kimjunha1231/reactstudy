de1120d6911a4b78ddfd9a5d9f323fcc14f9a696
junha kim <tto9011@naver.com>
2023-09-03 02:08:28 +0900

    dd

--- src/section3/Expenses/Expenses.js
+++ src/section3/Expenses/Expenses.js
@@ -1,17 +1,30 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpenseFilter";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020')
+  const [filteredYear, setFilteredYear] = useState("2020");
+  let filterInfoText = "2019, 2021 & 2022";
+
+  if (filteredYear === "2019") {
+    filterInfoText("2020, 2021 & 2022");
+  } else if (filteredYear === "2021") {
+    filterInfoText("2019, 2021 & 2022");
+  } else {
+    filterInfoText("2019, 2021 & 2022");
+  }
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <p>Data for years {filterInfoText} is hidden.</p>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}

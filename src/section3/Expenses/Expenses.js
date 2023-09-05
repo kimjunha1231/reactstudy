f5bf43cade93fe021522654ecb7f8a30a3386925
junha kim <tto9011@naver.com>
2023-09-05 11:45:20 +0900

    화팅

--- src/section3/Expenses/Expenses.js
+++ src/section3/Expenses/Expenses.js
@@ -6,25 +6,31 @@ import Card from "../UI/Card";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  let filterInfoText = "2019, 2021 & 2022";
 
-  if (filteredYear === "2019") {
-    filterInfoText("2020, 2021 & 2022");
-  } else if (filteredYear === "2021") {
-    filterInfoText("2019, 2021 & 2022");
-  } 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  let expensesContent = <p>No expenses found.</p>;
+  if(filteredExpenses.length>0){
+    expensesContent=filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>))}
-      
-
+      {expensesContent}
     </Card>
   );
 };

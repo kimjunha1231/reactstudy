8f29f688fc33e9ff49b4ffa0f6a54232b8c736a0
junha kim <tto9011@naver.com>
2023-09-06 10:44:58 +0900

    d

--- src/section3/Expenses/ExpensesList.js
+++ src/section3/Expenses/ExpensesList.js
@@ -5,6 +5,7 @@ const ExpensesList = (props) => {
   if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
   }
+  
   return (
     <ul className="expenses-list">
       {props.items.map((expense) => (
--- src/section3/NewExpense/ExpenseForm.js
+++ src/section3/NewExpense/ExpenseForm.js
@@ -89,7 +89,10 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button>Add Expense</button>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
